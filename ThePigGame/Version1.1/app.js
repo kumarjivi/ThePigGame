e3b374ac959ac46129bf7ccaee9f9fa2fd1686e0
Kumar Jivi <kumarjivi@yahoo.com>
2018-06-03 18:27:28 -0700

    Added code for version 1.1 of the game.

--- ThePigGame/Version1.1/app.js
+++ ThePigGame/Version1.1/app.js
@@ -4,11 +4,12 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn.
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer, gameInProgress;
+var scores, roundScore, activePlayer, gameInProgress, prevDice;
 
 init();
 
@@ -17,20 +18,27 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		//generate a random number.
 		var dice = Math.ceil(Math.random() * 6);
 		//display the result.
+		//dice = 6;
 		var diceButton = document.querySelector('.dice');
 		diceButton.style.display = 'block';
 		diceButton.src = 'dice-' + dice + '.png';
 		//update the result if rolled number is not 1.
 		if(dice > 1) {
-			roundScore += dice;
+			//check if this roll is 6 and the last roll was 6 as well.
+			if(dice == 6 && prevDice == 6) {
+				roundScore = 0;
+				prevDice = 0;
+				scores[activePlayer] = 0;
+				document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+				//give up current player's turn.
+				toggleActivePlayer();
+			} else {
+				prevDice = dice;
+				roundScore += dice;				
+			}
 			document.getElementById('current-' + activePlayer).textContent = roundScore;
 		} else {
-			roundScore = 0;
-			document.getElementById('current-' + activePlayer).textContent = roundScore;
-			//update active player and inactive player.
 			toggleActivePlayer();
-			//hide the dice.
-			//document.querySelector('.dice').style.display = 'none';
 		}
 	}
 	
@@ -59,6 +67,7 @@ document.querySelector('.btn-new').addEventListener('click', init);
 
 function init() {
 	//reset all scores.
+	prevDice = 0;
 	scores = [0,0];
 	roundScore = 0;
 	activePlayer = 0;
@@ -85,6 +94,8 @@ function init() {
 }
 
 function toggleActivePlayer() {
+	roundScore = 0;
+	document.getElementById('current-' + activePlayer).textContent = roundScore;
 	document.querySelector('.player-0-panel').classList.toggle('active');
 	document.querySelector('.player-1-panel').classList.toggle('active');
 	activePlayer = (activePlayer + 1)%2;
