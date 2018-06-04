d6011e9ce84ce2d78087400786f672c4677359fa
Kumar Jivi <kumarjivi@yahoo.com>
2018-06-03 17:49:31 -0700

    Renamed Basic version. Created a new Version

---
+++ ThePigGame/Version1.1/app.js
@@ -0,0 +1,98 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+*/
+
+var scores, roundScore, activePlayer, gameInProgress;
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	if(gameInProgress) {
+		//generate a random number.
+		var dice = Math.ceil(Math.random() * 6);
+		//display the result.
+		var diceButton = document.querySelector('.dice');
+		diceButton.style.display = 'block';
+		diceButton.src = 'dice-' + dice + '.png';
+		//update the result if rolled number is not 1.
+		if(dice > 1) {
+			roundScore += dice;
+			document.getElementById('current-' + activePlayer).textContent = roundScore;
+		} else {
+			roundScore = 0;
+			document.getElementById('current-' + activePlayer).textContent = roundScore;
+			//update active player and inactive player.
+			toggleActivePlayer();
+			//hide the dice.
+			//document.querySelector('.dice').style.display = 'none';
+		}
+	}
+	
+	
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	//hide the dice.
+	document.querySelector('.dice').style.display = 'none';
+	//update current score of active player.
+	updateScore(activePlayer);
+	if(gameInProgress) {
+		if(scores[activePlayer] >= 100) {
+			document.getElementById('name-'+activePlayer).textContent = "Winner!!!";
+			document.querySelector('.player-'+activePlayer +'-panel').classList.remove('active');
+			document.querySelector('.player-'+activePlayer +'-panel').classList.add('winner');
+			gameInProgress = false;
+		} else {
+			
+			toggleActivePlayer();
+		}		
+	}
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+	//reset all scores.
+	scores = [0,0];
+	roundScore = 0;
+	activePlayer = 0;
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	//make Player 1 active and Player 2 not active.
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+	//remove winner class from both players.
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	//show both hold and roll-dice buttons.
+	document.querySelector('.btn-hold').style.display = 'block';
+	document.querySelector('.btn-roll').style.display = 'block';
+	//reset the names of both players.
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+	//hide the dice
+	document.querySelector('.dice').style.display = 'none';
+	gameInProgress = true;
+}
+
+function toggleActivePlayer() {
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+	activePlayer = (activePlayer + 1)%2;
+}
+
+function updateScore(playerId) {
+	scores[playerId] += roundScore;
+	roundScore = 0;
+	document.getElementById('current-' + playerId).textContent = '0';
+	document.getElementById('score-' + playerId).textContent = scores[playerId];
+}
